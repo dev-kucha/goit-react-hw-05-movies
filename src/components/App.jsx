33716b9fb760197e084be08e8dfb35fa0265dfe0
Yurii Kucher <ukucher@gmail.com>
2022-09-18 23:31:16 +0300

    some bugs fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,16 +17,7 @@ const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
+    <div>
       <>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
--- src/components/GoBack/GoBack.jsx
+++ src/components/GoBack/GoBack.jsx
@@ -3,16 +3,12 @@ import { useNavigate } from 'react-router-dom';
 export default function GoBack({ from }) {
   const navigate = useNavigate();
 
-  //   console.log(from);
-
   const handleClick = () => {
-    // console.log('cklickded');
     navigate(from, { replace: true });
   };
 
   return (
     <button type="button" onClick={handleClick}>
-      {' '}
       &#x27F5; Go back
     </button>
   );
